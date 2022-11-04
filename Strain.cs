b19e2dfb47ae2c71588636aaa319f3aa0b546758
Vshah-1 <94983341+Vshah-1@users.noreply.github.com>
2022-11-04 11:48:03 +0000

    Add files via upload

---
+++ Strain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StrainExercise
+{
+
+    public static class Strain
+    {
+
+        // Class with two methods: Keep and Discard
+        // Keep and Discard are both lists of Ienumerables, this is to hold the values for the methods,
+            // What we want to keep, and what we want to discard 
+        // Within the method parameters are passed an identical list called collection
+            // Within collection, we decide which values go into Keep, and which go into Discard 
+        // We decide what comes out of collection into a method by,
+            // assessing how the data fares with the Function predicate
+        // predicate Function has a bool output, and a generic input 
+        public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            List<T> keep = new List<T>();
+            var i = 0;
+            foreach (var item in collection)
+            {
+                if (predicate(item).Equals(true))
+                {
+                    keep.Add(item);
+                    i++;
+                }
+
+            }
+            return keep;
+        }
+
+        public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+
+            List<T> discard = new List<T>();
+            var i = 0;
+            foreach (var item in collection)
+            {
+                if (predicate(item).Equals(false))
+                {
+                    discard.Add(item);
+                    i++;
+                }
+
+            }
+            return discard;
+        }
+    }
+}
---
+++ StrainTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StrainExercise;
+
+namespace Strain
+{
+    internal class StrainTests
+    {
+        [Test]
+        public void Empty_keep()
+        {
+            Assert.AreEqual(new LinkedList<int>(), new LinkedList<int>().Keep(x => x < 10));
+        }
+        [Test]
+        public void Keep_everything()
+        {
+            Assert.AreEqual(new HashSet<int> { 1, 2, 3 }, new HashSet<int> { 1, 2, 3 }.Keep(x => x < 10));
+        }
+        [Test]
+        public void Keep_first_and_last()
+        {
+            Assert.AreEqual(new[] { 1, 3 }, new[] { 1, 2, 3 }.Keep(x => x % 2 != 0));
+        }
+        [Test]
+        public void Keep_neither_first_nor_last()
+        {
+            Assert.AreEqual(new List<int> { 2, 4 }, new List<int> { 1, 2, 3, 4, 5 }.Keep(x => x % 2 == 0));
+        }
+        [Test]
+        public void Keep_strings()
+        {
+            var words = "apple zebra banana zombies cherimoya zelot".Split(' ');
+            Assert.AreEqual("zebra zombies zelot".Split(' '), words.Keep(x => x.StartsWith("z")));
+        }
+        [Test]
+        public void Keep_arrays()
+        {
+            var actual = new[]
+                {
+            new[] { 1, 2, 3 },
+            new[] { 5, 5, 5 },
+            new[] { 5, 1, 2 },
+            new[] { 2, 1, 2 },
+            new[] { 1, 5, 2 },
+            new[] { 2, 2, 1 },
+            new[] { 1, 2, 5 }
+        };
+            var expected = new[] { new[] { 5, 5, 5 }, new[] { 5, 1, 2 }, new[] { 1, 5, 2 }, new[] { 1, 2, 5 } };
+            Assert.AreEqual(expected, actual.Keep(x => x.Contains(5)));
+        }
+        [Test]
+        public void Empty_discard()
+        {
+            Assert.AreEqual(new LinkedList<int>(), new LinkedList<int>().Discard(x => x < 10));
+        }
+        [Test]
+        public void Discard_nothing()
+        {
+            Assert.AreEqual(new HashSet<int> { 1, 2, 3 }, new HashSet<int> { 1, 2, 3 }.Discard(x => x > 10));
+        }
+        [Test]
+        public void Discard_first_and_last()
+        {
+            Assert.AreEqual(new[] { 2 }, new[] { 1, 2, 3 }.Discard(x => x % 2 != 0));
+        }
+        [Test]
+        public void Discard_neither_first_nor_last()
+        {
+            Assert.AreEqual(new List<int> { 1, 3, 5 }, new List<int> { 1, 2, 3, 4, 5 }.Discard(x => x % 2 == 0));
+        }
+        [Test]
+        public void Discard_strings()
+        {
+            var words = "apple zebra banana zombies cherimoya zelot".Split(' ');
+            Assert.AreEqual("apple banana cherimoya".Split(' '), words.Discard(x => x.StartsWith("z")));
+        }
+        [Test]
+        public void Discard_arrays()
+        {
+            var actual = new[]
+                {
+            new[] { 1, 2, 3 },
+            new[] { 5, 5, 5 },
+            new[] { 5, 1, 2 },
+            new[] { 2, 1, 2 },
+            new[] { 1, 5, 2 },
+            new[] { 2, 2, 1 },
+            new[] { 1, 2, 5 }
+        };
+            var expected = new[] { new[] { 1, 2, 3 }, new[] { 2, 1, 2 }, new[] { 2, 2, 1 } };
+            Assert.AreEqual(expected, actual.Discard(x => x.Contains(5)));
+        }
+    }
+}
